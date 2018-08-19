2c52154f0c7853b25a5aac356244d73946e557ad
Việt Thảo <t.to.the.rekt@gmail.com>
2018-08-19 15:48:41 +0700

    Remove test code

--- modules/analyzer/referralController.js
+++ modules/analyzer/referralController.js
@@ -1,4 +1,3 @@
-// require('dotenv').config({ path: __dirname + '/../../.env' })
 const referralModel = require('./referralModel.js');
 const utils = require('../utils');
 const analyzerController = require('./analyzerController.js');
@@ -6,31 +5,6 @@ const mongoose = require('mongoose');
 const kidsModel = require('./kidsModel.js');
 const rewardConfigModel = require('./rewardConfigModel.js');
 const _ = require('lodash')
-
-// dummy_user = {
-//     "course": {
-//         "slug": "hoc-lap-trinh-tu-co-ban-hoc-sinh-cap-ba"
-//     },
-//     "kid": {
-//         "facebook": "https://facebook.com/lina.vu.97",
-//         "phone": "01672075654",
-//         "mail": "huynhtuanhuy19896@gmail.com",
-//         "institude": "asdasd",
-//         "dob": "2018-07-18T12:00:00+07:00",
-//         "name": {"first": "Test", "last": "Test"},
-//         "experience": "sadassadds",
-//         "inviteBy": "https://facebook.com/100013265649303"
-//     },
-//     "_id": "5b553034cdae66b3fd7c0dd6",
-//     "connectBy": "Google",
-//     "method": "Registration by Website",
-//     "utm_campaign": "",
-//     "utm_medium": "",
-//     "utm_source": "",
-//     "createdAt": "2018-07-23T01:32:36.284Z",
-//     "updatedAt": "2018-07-23T01:32:36.284Z",
-//     "__v": 0
-// }
 
 addNewRef = (user) => {
   fb_url = user.kid.inviteBy
@@ -58,7 +32,7 @@ addNewRef = (user) => {
                 reward: 0
               }] 
             };
-            
+
             return reward;
           }
 
@@ -91,14 +65,5 @@ addNewRef = (user) => {
       else console.log(err);
     });
 }
-
-// mongoose.connect(process.env.dburi)
-//   .then(() => {
-//     console.log('DB connected')
-//     addNewRef(dummy_user).then(resp => console.log(resp))
-//   })
-//   .catch(err => {
-//     console.log(err)
-//   })
 
 module.exports = { addNewRef }
